31262fd0f5667bafb117070c21495e53fd43aa58
ledaiduongmoscowrussia <ledaiduongvnth@gmail.com>
2018-03-23 09:02:32 +0300

    aaa

--- CheckScoreTin/gce/startup-script.sh
+++ CheckScoreTin/gce/startup-script.sh
@@ -1,3 +1,4 @@
+
 #! /bin/bash
 # Copyright 2015 Google Inc.
 #
@@ -13,14 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# [START startup]
 set -v
-
-# Talk to the metadata server to get the project id
 PROJECTID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
-
-# Install logging monitor. The monitor will automatically pickup logs sent to
-# syslog.
 # [START logging]
 curl -s "https://storage.googleapis.com/signals-agents/logging/google-fluentd-install.sh" | bash
 service google-fluentd restart &
@@ -28,31 +23,31 @@ service google-fluentd restart &
 
 # Install dependencies from apt
 apt-get update
+alias python=python3
 apt-get install -yq \
     git build-essential supervisor python python-dev python-pip libffi-dev \
     libssl-dev
 
-# Create a pythonapp user. The application will run as this user.
+#................................ Create a pythonapp user. The application will run as this user.
 useradd -m -d /home/pythonapp pythonapp
 
-# pip from apt is out of date, so make it update itself and install virtualenv.
-pip install --upgrade pip virtualenv
 
-# Get the source code from the Google Cloud Repository
-# git requires $HOME and it's not set during the startup script.
+#................................ Get the source code from the Google Cloud Repository
 export HOME=/root
+rm -r /opt/app
 git config --global credential.helper gcloud.sh
 git clone https://source.developers.google.com/p/$PROJECTID/r/HelloWorld /opt/app
 
-# Install app dependencies
+#................................ Install app dependencies
 virtualenv -p python3 /opt/app/CheckScoreTin/env
+#................................ pip from apt is out of date, so make it update itself and install virtualenv.
+pip install --upgrade pip virtualenv
 /opt/app/CheckScoreTin/env/bin/pip install -r /opt/app/CheckScoreTin/requirements.txt
 
-# Make sure the pythonapp user owns the application code
+#................................ Make sure the pythonapp user owns the application code
 chown -R pythonapp:pythonapp /opt/app
 
-# Configure supervisor to start gunicorn inside of our virtualenv and run the
-# application.
+#................................ Configure supervisor to start gunicorn inside of our virtualenv and run the
 cat >/etc/supervisor/conf.d/python-app.conf << EOF
 [program:pythonapp]
 directory=/opt/app/CheckScoreTin
@@ -71,5 +66,5 @@ EOF
 supervisorctl reread
 supervisorctl update
 
-# Application should now be running under supervisor
-# [END startup]
+#................................ Application should now be running under supervisor
+#................................ [END startup]
