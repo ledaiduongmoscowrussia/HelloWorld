46c5b6012b7508a4d26a4c8a5fd5c123c2d008f4
ledaiduongmoscowrussia <ledaiduongvnth@gmail.com>
2018-04-05 09:24:26 +0300

    sfsdf

--- CheckScoreTin/LayoutWebApplication.py
+++ CheckScoreTin/LayoutWebApplication.py
@@ -53,7 +53,7 @@ layout_table_results = html.Div([dcc.Dropdown(id='subject_to_show_table', option
                                                'qs21', 'qs22', 'qs23', 'qs24', 'qs25', 'qs26', 'qs27', 'qs28', 'qs29', 'qs30',
                                                'qs31', 'qs32', 'qs33', 'qs34', 'qs35', 'qs36', 'qs37', 'qs38', 'qs39', 'qs40',
                                                'qs41', 'qs42', 'qs43', 'qs44', 'qs45', 'qs46', 'qs47', 'qs48', 'qs49', 'qs50'],
-                                     column_widths= np.full([1, 51], 10).tolist()[0],
+                                     column_widths= np.full([1, 51], 100).tolist()[0],
                                      id='table_result')
                                  ])
 
